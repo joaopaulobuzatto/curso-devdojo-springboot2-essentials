bec78e35574d88a2ec3241fcc165223f097cacf4
joaopaulobuzatto <joaopaulobuzatto@gmail.com>
2021-09-20 15:17:38 -0300

    [MASTER] - Commit aula 28 - RestTemplate POST.

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -3,8 +3,7 @@ package academy.devdojo.springboot2.client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -24,11 +23,28 @@ public class SpringClient {
         log.info(Arrays.toString(animes));
 
         //@formatter:off
-        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+        ResponseEntity<List<Anime>> exchangeGetAll = new RestTemplate().exchange("http://localhost:8080/animes/all",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<>() {});
         //@formatter:on
-        log.info(exchange.getBody());
+        log.info(exchangeGetAll.getBody());
+
+//        Anime postRequest = Anime.builder().name("Post Request").build();
+//        Anime postResponse = new RestTemplate().postForObject("http://localhost:8080/animes", postRequest, Anime.class);
+//        log.info(postResponse);
+
+        Anime postRequest = Anime.builder().name("Post Request Exchange 2").build();
+        ResponseEntity<Anime> exchangePost = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(postRequest, createJsonHeader()),
+                Anime.class);
+        log.info(exchangePost);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
