de025237f41e5a03f7c31157ce3ccdcdedfecf9d
joaopaulobuzatto <joaopaulobuzatto@gmail.com>
2021-10-03 05:09:39 -0300

    [MASTER] - Commit aula 47 - Documentação com SpringDoc OpenAPI pt 02.

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,12 +1,12 @@
 package academy.devdojo.springboot2.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
@@ -14,6 +14,6 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
-    @NotNull(message = "The anime name cannot be null")
+    @Schema(description = "This is the Anime's name", example = "New Name", required = true)
     private String name;
 }
