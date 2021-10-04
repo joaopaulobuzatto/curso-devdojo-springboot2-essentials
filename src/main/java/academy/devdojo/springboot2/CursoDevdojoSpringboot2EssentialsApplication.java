975bead4bc78ca90d010ba7199f488774b765e8f
joaopaulobuzatto <joaopaulobuzatto@gmail.com>
2021-10-04 06:03:00 -0300

    [MASTER] - Commit aula 50 - Monitorando métricas com gráficos no Grafana.

--- src/main/java/academy/devdojo/springboot2/CursoDevdojoSpringboot2EssentialsApplication.java
+++ src/main/java/academy/devdojo/springboot2/CursoDevdojoSpringboot2EssentialsApplication.java
@@ -1,7 +1,11 @@
 package academy.devdojo.springboot2;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CursoDevdojoSpringboot2EssentialsApplication {
@@ -10,4 +14,9 @@ public class CursoDevdojoSpringboot2EssentialsApplication {
         SpringApplication.run(CursoDevdojoSpringboot2EssentialsApplication.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
